440b210362c5134b783a23de03aab85ac2807a52
Slav Lomachynkyi <slavchek.3777@gmail.com>
2023-08-10 00:44:35 +0300

    work with register

--- Frontend/src/components/RegisterBox/index.tsx
+++ Frontend/src/components/RegisterBox/index.tsx
@@ -8,7 +8,7 @@ const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const RegisterBox = () => {
   const userRef = useRef<HTMLInputElement>(null);
-  const errrRef = useRef<HTMLParagraphElement>(null);
+  const errRef = useRef<HTMLInputElement>(null);
 
   const [user, setUser] = useState("");
   const [validName, setValidName] = useState(false);
@@ -33,15 +33,11 @@ const RegisterBox = () => {
 
   useEffect(() => {
     const result = userRegex.test(user);
-    console.log(result);
-    console.log(user);
-    console.log(result);
+    setValidName(result);
   }, [user]);
 
   useEffect(() => {
     const result = pwdRegex.test(pwd);
-    console.log(result);
-    console.log(pwd);
     setValidPwd(result);
     const match = pwd === matchPwd;
     setValidMatch(match);
@@ -51,104 +47,129 @@ const RegisterBox = () => {
     setErrMsg("");
   }, [user, pwd, matchPwd]);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setSuccess(true);
+  };
+
   return (
-    <section className="centered-content">
-      <div className="register-form">
-        <p
-          ref={errrRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
-        <h1>Register</h1>
-        <form>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            ref={userRef}
-            autoComplete="off"
-            onChange={(e) => setUser(e.target.value)}
-            required
-            aria-invalid={validName ? "false" : "true"}
-            aria-describedby="uidnote"
-            onFocus={() => setUserFocus(true)}
-            onBlur={() => setUserFocus(false)}
-          ></input>
-          <p
-            id="uidnote"
-            className={
-              userFocus && user && !validName ? "instructions" : "offscreen"
-            }
-          >
-            4 to 24 characters.
-            <br />
-            Must begin with a letter.
-            <br />
-            Letters, numbers, underscores, hyphens allowed.
+    <>
+      {success ? (
+        <section>
+          <h1>Success</h1>
+          <p>
+            <Link to="/">Look Posts</Link>
           </p>
+        </section>
+      ) : (
+        <section className="centered-content">
+          <div className="register-form">
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+              ></input>
+              <p
+                id="uidnote"
+                className={
+                  userFocus && user && !validName ? "instructions" : "offscreen"
+                }
+              >
+                4 to 24 characters.
+                <br />
+                Must begin with a letter.
+                <br />
+                Letters, numbers, underscores, hyphens allowed.
+              </p>
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-            required
-            aria-invalid={validPwd ? "false" : "true"}
-            aria-describedby="pwdnote"
-            onFocus={() => setPwdFocus(true)}
-            onBlur={() => setPwdFocus(false)}
-          />
-          <p
-            id="pwdnote"
-            className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
-          >
-            8 to 24 characters.
-            <br />
-            Must include uppercase and lowercase letters, a number and a special
-            character.
-            <br />
-            Allowed special characters:{" "}
-            <span aria-label="exclamation mark">!</span>{" "}
-            <span aria-label="at symbol">@</span>{" "}
-            <span aria-label="hashtag">#</span>{" "}
-            <span aria-label="dollar sign">$</span>{" "}
-            <span aria-label="percent">%</span>
-          </p>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                aria-invalid={validPwd ? "false" : "true"}
+                aria-describedby="pwdnote"
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+              <p
+                id="pwdnote"
+                className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+              >
+                8 to 24 characters.
+                <br />
+                Must include uppercase and lowercase letters, a number and a
+                special character.
+                <br />
+                Allowed special characters:{" "}
+                <span aria-label="exclamation mark">!</span>{" "}
+                <span aria-label="at symbol">@</span>{" "}
+                <span aria-label="hashtag">#</span>{" "}
+                <span aria-label="dollar sign">$</span>{" "}
+                <span aria-label="percent">%</span>
+              </p>
 
-          <label htmlFor="confirm_pwd">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirm_pwd"
-            onChange={(e) => setMatchPwd(e.target.value)}
-            value={matchPwd}
-            required
-            aria-invalid={validMatch ? "false" : "true"}
-            aria-describedby="confirmnote"
-            onFocus={() => setMatchFocus(true)}
-            onBlur={() => setMatchFocus(false)}
-          />
-          <p
-            id="confirmnote"
-            className={matchFocus && !validMatch ? "instructions" : "offscreen"}
-          >
-            Must match the first password input field.
-          </p>
+              <label htmlFor="confirm_pwd">Confirm Password:</label>
+              <input
+                type="password"
+                id="confirm_pwd"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                aria-invalid={validMatch ? "false" : "true"}
+                aria-describedby="confirmnote"
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+              <p
+                id="confirmnote"
+                className={
+                  matchFocus && !validMatch ? "instructions" : "offscreen"
+                }
+              >
+                Must match the first password input field.
+              </p>
 
-          <button
-            className="submit-button"
-            disabled={!validName || !validPwd || !validMatch ? true : false}
-          >
-            Sign Up
-          </button>
-          <button className="submit-button">
-            <Link to="/login">Sign In</Link>
-          </button>
-        </form>
-      </div>
-    </section>
+              <button
+                type="submit"
+                disabled={!validName || !validPwd || !validMatch ? true : false}
+                className="submit-button"
+              >
+                Sign Up
+              </button>
+            </form>
+            <p>
+              Already registered?
+              <br />
+              <span className="line">
+                <Link to="/login">Sign In</Link>
+              </span>
+            </p>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
